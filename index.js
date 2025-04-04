5bf03f196e1b3244c656a9171911727adf2ea17b
numbundupbap <hjsmart38@gmail.com>
2025-04-05 08:34:07 +0900

    🤕

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 import fs from "fs"
 import { parseEmoji } from "./emoji_parser.js"
+import { type } from "os";
 
 const data = fs.readFileSync("🤔.🤓😏👩‍💻", { encoding: "utf8" })
 const dataMap = parseEmoji(data)
 
-console.log(dataMap)
+let tokens = []; 
+
+dataMap.forEach((s) => {
+    let code = '';
+    s.forEach((c, i) => {
+        code += c;
+        if (code == '☝️🤓') {
+            tokens.push({type: "label", params: [s[i+1]]});
+        } else if (code == '🤕🪨') {
+            tokens.push({type: "start", params: []});
+        } else if (code == '🤠🥕') {
+            tokens.push({type: "exit", params: []});
+        } else if (code == '📦') {}
+    })
+});
+
+console.log(tokens)
