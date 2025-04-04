b492d98e39fb70558231311d112ca943714f9f55
ChessBot-Entry <chessbot.entry@gmail.com>
2025-04-04 18:51:27 +0900

    🗿

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import fs from "fs"
 
 const data = fs.readFileSync("🤔.🤓😏👩‍💻", {encoding:"utf8"})
-const dataArray = Array.from(data)
+let dataArray = Array.from(data)
 
-console.log(data)
+console.log(dataArray)
 console.log(dataArray[0])
+
+dataArray.filter(chr => {
+  if (/\p{Emoji}/gu.test(chr)) return true
+  if (chr === '\n') return true
+  if (/[\s\uFE0F\u200D]/.test(chr)) return false
+
+  throw new Error(`🤦🖕🔤${chr.codePointAt()}`)
+})
